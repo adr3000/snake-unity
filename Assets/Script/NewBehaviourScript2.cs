ef7a1fe730d770f4a3ec13b4a00274c67ee21055
ad <ad.lartificier@gmail.com>
2020-09-28 15:32:35 -0300

    first commit

---
+++ Assets/Script/NewBehaviourScript2.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class NewBehaviourScript2 : MonoBehaviour {
+	
+	bool ate = false;
+
+	
+	bool isDied = false;
+
+	
+	public GameObject tailPrefab;
+
+	
+	Vector2 dir = Vector2.right;
+
+	
+	List<Transform> tail = new List<Transform>();
+
+	
+	void Start () {
+	
+		InvokeRepeating("Move", 0.2f, 0.2f); 
+	}
+
+	
+	void Update () {
+		if (!isDied) {
+			
+			if (Input.GetKey (KeyCode.RightArrow))
+				dir = Vector2.right;
+			else if (Input.GetKey (KeyCode.DownArrow))
+				dir = -Vector2.up;    
+			else if (Input.GetKey (KeyCode.LeftArrow))
+				dir = -Vector2.right; 
+			else if (Input.GetKey (KeyCode.UpArrow))
+				dir = Vector2.up;
+		} else {
+			if (Input.GetKey(KeyCode.R)){
+				
+				tail.Clear();
+
+				
+				transform.position = new Vector3(0, 0, 0);
+
+				
+				isDied = false;
+			}
+		}
+	}
+
+	void Move() {
+		if (!isDied) {
+			
+			Vector2 v = transform.position;
+
+			
+			transform.Translate (dir);
+
+			
+			if (ate) {
+				
+				GameObject g = (GameObject)Instantiate (tailPrefab,
+					              v,
+					              Quaternion.identity);
+
+				
+				tail.Insert (0, g.transform);
+
+				
+				ate = false;
+			} else if (tail.Count > 0) {	
+					
+					tail.Last ().position = v;
+
+					
+					tail.Insert (0, tail.Last ());
+					tail.RemoveAt (tail.Count - 1);
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		
+		if (coll.name.StartsWith("Food")) {
+			
+			ate = true;
+
+			
+			Destroy(coll.gameObject);
+		} else { 	
+			isDied = true;
+		}
+	}
+}
+ 
